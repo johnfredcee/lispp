0475f2d4b1f67e54d718a65602feb9241eeba418
John Connors <johnc@yagc.ndo.co.uk>
2015-12-18 07:01:54 +0000

    Initial work on evaluating lambdas

--- LispEval.cpp
+++ LispEval.cpp
@@ -32,20 +32,29 @@ namespace Lisp {
 		LispObjRef fnsym(car(obj));
 		if (is_symbol(fnsym) || is_primitive(fnsym))
 		{	
-			LispObjRef fn = is_symbol(fnsym) ? env->fref(get_ctype<SymbolType>(fnsym).name) : fnsym;
+			LispObjRef fn = is_symbol(fnsym) ? env->fref(get_ctype<SymbolType>(fnsym).name) : fnsym; // 
 			// it's a function
 			if (is_primitive(fn)) {
 				CPrim cfn = (CPrim)(boost::get<PrimType>(*fn));
 				// call it on the cdr
 				return cfn(cdr(obj), env);
-			} else {
-				// Else .. it's a lambda..
-				if (is_cons(fn)) {
-					LispObjRef args = car(fn);
-					LispObjRef body = cdr(fn);
-					// extend env, bind (cdr obj to args), eval body with new env
-					// ..
-				}
+			}
+			// lambda is represented by a cons ((args) (body))
+			if (is_cons(fn)) {
+				LispObjRef params = car(fn);
+				LispObjRef body = cdr(fn);
+				LispEnvRef lambda_env(new LispEnv(env));
+				LispObjRef param;
+				LispObjRef arg;
+				LispObjRef args = cadr(obj);
+				do {
+					param = car(params);
+					arg = eval(car(args));
+					lambda_env->set(get_ctype<SymbolType>(param).name, arg);
+					param = cadr(params);
+					arg = is_nil(arg) ? nil : cadr(args);
+				} while (!is_nil(param));
+				eval(body, lambda_env);
 			}
 		}
 	} 
